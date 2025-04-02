c5c3a470f2cb8508253736fa4f5399b25955d95a
romain <castelao.romain@gmail.com>
2025-04-02 16:24:32 +0200

    début du header

--- src/App.jsx
+++ src/App.jsx
@@ -2,34 +2,22 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import myData from './romain.json'
+
+function Header({firstname, lastname, description}){
+  return (
+    <header>
+      <div className="flex flex-col items-center justify-center bg-[#FD4245]">
+        <h1 className="text-5xl">{lastname} {firstname}</h1>
+        <p>{description}</p>
+      </div>
+    </header>
+  )
+}
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  return <Header firstname={myData.firstname} lastname={myData.lastname} description="ÉTUDIANT EN INFORMATIQUE À LA RECHERCHE D'UN STAGE
+  DE 18 SEMAINES MINIMUM À PARTIR DE JANVIER 2025"/>;
 }
 
 export default App
